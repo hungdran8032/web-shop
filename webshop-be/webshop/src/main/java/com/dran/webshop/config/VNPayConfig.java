6c1a95750c9c07e08613a35e94c94961196cd5dd
hungdran8032 <143425730+hungdran8032@users.noreply.github.com>
2025-03-25 00:23:41 +0700

    khong biet noi gi

--- webshop-be/webshop/src/main/java/com/dran/webshop/config/VNPayConfig.java
+++ webshop-be/webshop/src/main/java/com/dran/webshop/config/VNPayConfig.java
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.Configuration;
 public class VNPayConfig {
     public static final String VNP_PAY_URL = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
     public static final String VNP_RETURN_URL = "http://localhost:8080/payment/vnpay/return";
-    public static final String VNP_TMN_CODE = "NHWVJ7DL";
-    public static final String VNP_HASH_SECRET = "XJHKIE2RL0TNVT9ESJ141OFE6I097FSA";
+    public static final String VNP_TMN_CODE = "";
+    public static final String VNP_HASH_SECRET = "";
     public static final String VNP_VERSION = "2.1.0";
     public static final String VNP_COMMAND = "pay";
     public static final String VNP_CURR_CODE = "VND";
